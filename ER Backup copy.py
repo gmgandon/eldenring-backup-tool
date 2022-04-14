60e66b92aff5bdbd17bd03a431d5e4587c908b21
Guido Gandón <gmgandonmusic@gmail.com>
2022-04-14 05:32:50 -0300

    add settings.txt for storing paths

--- ER Backup copy.py
+++ ER Backup copy.py
@@ -26,42 +26,63 @@ defaultpath = (os.path.expanduser('~')) + '\Appdata\Roaming\EldenRing'
 #exit()
 
 #directory settings:
-if path.exists('\settings.txt') == False:
-    with open ("\settings.txt", "w") as settings:
+if path.exists('settings.txt') == False:
+    with open ("settings.txt", "w") as settings:
         settings.write('\n\n')
         erpath = ''
         destpath = ''
 else:
-    with open ('\settings.txt', 'r') as settings:
+    with open ('settings.txt', 'r') as settings:
         data= settings.readlines()
         erpath = data[0].replace('\n','')
         destpath = data[1].replace('\n','')
 
     
 
-#datos de la ventana
-root= tk.Tk()
-root.title("Elden Ring Backup tool")
-root.config(width=300, height=250)
-root.resizable(False , False)
-root.option_add('*tearOff', False)
+
+
+#datos de settings
+
 
 
 
 def getfilename():
-    defaultname= 'ER0000'
+    deftaultname= 'ER0000'
     description = description_box.get()
 
     if description != '' :
-        return defaultname + description
+        return description
     else:
-        return defaultname + (time.asctime().replace(":", "-"))
+        return deftaultname + (time.asctime().replace(":", "-"))
 
 def setts():
     settingswindow = Toplevel()
     settingswindow.title('Settings')
     settingswindow.geometry('300x200')
-    pass
+
+    #box y boton origen
+    origin_label= tk.Label(settingswindow, text='Save file:')
+    origin_label.place(x=20, y=70)
+
+    origin_box= ttk.Entry(settingswindow)
+    origin_box.place(x=20, y=100, width=200)
+    origin_box.insert(0, origindir())
+
+    botonorigin = ttk.Button(settingswindow, text="...", command=save_file)
+    botonorigin.place(x= 250, y= 98, width=25)
+
+
+    #destino
+    destination_label = tk.Label(settingswindow, text='backup location')
+    destination_label.place(x=20, y=130)
+
+    destination_box= ttk.Entry(settingswindow)
+    destination_box.place(x=20, y=160, width=200)
+    destination_box.insert(0, destinationdir())
+
+    botondestination = ttk.Button(settingswindow, text="...", command=backup_file)
+    botondestination.place(x= 250, y= 158, width=25)
+
     
 def origindir():
     if erpath == '':
@@ -89,8 +110,7 @@ def save_file():
         title='Selected file',
         message=filename
     )
-    origin_box.delete(0, END)
-    origin_box.insert(0, filename)
+
 
 def backup_file():
     filename = asksaveasfilename(
@@ -105,20 +125,23 @@ def backup_file():
         title='Selected file',
         message=filename
     )    
-    destination_box.delete(0, END)
-    destination_box.insert(0, filename)
 
 
 def backupSaveFile():
-    origin = origin_box.get()
-    #destination = f'{destination_box.get()[:-4]}{getfilename()}{destination_box.get()[-4::]}'
-    destination = destination_box.get()
+    origin = save_file
+    destination = backup_file
     print(origin, destination)
     boton_label.config(text= 'Done!')
-    with open ('\settings.txt', "w") as settings:
-        settings.write(f"{origin}\n{destination_box.get()}\n")
+    with open ('settings.txt', "w") as settings:
+        settings.write(f"{origin}\n{destination}\n")
 
 
+#datos de la ventana
+root= tk.Tk()
+root.title("Elden Ring Backup tool")
+root.config(width=300, height=250)
+root.resizable(False , False)
+root.option_add('*tearOff', False)
 
 #menu
 menubar = Menu(root)
@@ -130,7 +153,7 @@ menubar.add_cascade(menu=menu_file, label='File')
 #add dropdown items
 menu_file.add_command(label='Settings...', command=setts)
 menu_file.add_separator()
-#menubar.add_cascade(menu=menu_edit, label='Edit')
+
 
 
 #description
@@ -142,28 +165,6 @@ description_box.place(x=20, y=40, width=200)
 
 
     
-#box y boton origen
-origin_label= tk.Label(text='Save file:')
-origin_label.place(x=20, y=70)
-
-origin_box= ttk.Entry()
-origin_box.place(x=20, y=100, width=200)
-origin_box.insert(0, origindir())
-
-botonorigin = ttk.Button(text="...", command=save_file)
-botonorigin.place(x= 250, y= 98, width=25)
-
-
-#destino
-destination_label = tk.Label(text='backup location')
-destination_label.place(x=20, y=130)
-
-destination_box= ttk.Entry()
-destination_box.place(x=20, y=160, width=200)
-destination_box.insert(0, destinationdir())
-
-botondestination = ttk.Button(text="...", command=backup_file)
-botondestination.place(x= 250, y= 158, width=25)
 
 
 #boton
